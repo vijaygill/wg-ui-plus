a06f6e276bcf537ce46226de664ddd0a303da6e9
Vijay Gill <Vijay.Gill@hotmail.com>
2024-05-01 23:22:03 +0100

    More initial stuff.

--- app/app.py
+++ app/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask,render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='wg-ui-plus/dist/wg-ui-plus/browser')
+
+@app.route('/test')
+def test():
+    return 'Hello, World!'
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host = '0.0.0.0', port = 80)
