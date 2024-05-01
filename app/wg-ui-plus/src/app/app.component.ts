ef79900588dd3dbf241aa5380e8ada22820a177d
Vijay Gill <Vijay.Gill@hotmail.com>
2024-05-02 00:35:17 +0100

    Added PrimeNG controls and a sample button.

--- app/wg-ui-plus/src/app/app.component.ts
+++ app/wg-ui-plus/src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
+import { ButtonModule } from 'primeng/button';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
