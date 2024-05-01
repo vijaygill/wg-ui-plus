9f842a2c9f25d3ae71790b09c678ce74ac819593
Vijay Gill <Vijay.Gill@hotmail.com>
2024-05-01 23:36:28 +0100

    Mount-bind project folder so that git can access it within the container.

--- build-and-run-dev.sh
+++ build-and-run-dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t wg-ui-plus -f Dockerfile . && docker run -it --rm -p 13080:80 -v ./app:/app -v "${HOME}/.gitconfig":/root/.gitconfig:ro  wg-ui-plus bash
+docker build -t wg-ui-plus -f Dockerfile . && docker run -it --rm -p 13080:80 -v /home/pi/temp/wg-ui-plus:/wg-ui-plus -v "${HOME}/.gitconfig":/root/.gitconfig:ro wg-ui-plus bash
 
