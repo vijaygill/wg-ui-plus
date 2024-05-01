8d6009bc69b06e74cef412e2b87731c74c1e3827
Vijay Gill <Vijay.Gill@hotmail.com>
2024-05-01 23:52:41 +0100

    Added home component and it works!

--- app/wg-ui-plus/src/app/app.component.ts
+++ app/wg-ui-plus/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
