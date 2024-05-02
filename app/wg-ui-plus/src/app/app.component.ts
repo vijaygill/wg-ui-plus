9cdcdf61e9f8f067d28f70609baca1a8c06e8ba8
Vijay Gill <Vijay.Gill@hotmail.com>
2024-05-02 17:37:33 +0100

    More app related stuff. Added PrimeNG components.

--- app/wg-ui-plus/src/app/app.component.ts
+++ app/wg-ui-plus/src/app/app.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { PrimeNGModule } from './primeng.module';
+
+import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 
-import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, ButtonModule],
+  imports: [RouterOutlet, FormsModule, HomeComponent, AboutComponent, PrimeNGModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'wg-ui-plus';
+  pizza: string[] = [];
 }
---
+++ app/wg-ui-plus/src/app/primeng.module.ts
@@ -0,0 +1,68 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ConfirmationService } from 'primeng/api';
+
+//import { AccordionModule } from 'primeng/accordion';
+import { ButtonModule } from 'primeng/button';
+//import { CardModule } from 'primeng/card';
+import { CheckboxModule } from 'primeng/checkbox';
+//import { ConfirmDialogModule } from 'primeng/confirmdialog';
+//import { ConfirmPopupModule } from 'primeng/confirmpopup';
+//import { ContextMenuModule } from 'primeng/contextmenu';
+//import { DialogModule } from 'primeng/dialog';
+//import { DividerModule } from 'primeng/divider';
+//import { DragDropModule } from 'primeng/dragdrop';
+//import { DockModule } from 'primeng/dock';
+//import { DynamicDialogModule } from 'primeng/dynamicdialog';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+//import { KnobModule } from 'primeng/knob';
+//import { ListboxModule } from 'primeng/listbox';
+//import { MegaMenuModule } from 'primeng/megamenu';
+//import { MenuModule } from 'primeng/menu';
+//import { MenubarModule } from 'primeng/menubar';
+//import { MessagesModule } from 'primeng/messages';
+//import { MeterGroupModule } from 'primeng/metergroup';
+//import { MultiSelectModule } from 'primeng/multiselect';
+//import { OrderListModule } from 'primeng/orderlist';
+//import { OrganizationChartModule } from 'primeng/organizationchart';
+//import { PaginatorModule } from 'primeng/paginator';
+//import { PanelMenuModule } from 'primeng/panelmenu';
+//import { PanelModule } from 'primeng/panel';
+//import { PasswordModule } from 'primeng/password';
+//import { PickListModule } from 'primeng/picklist';
+//import { ProgressBarModule } from 'primeng/progressbar';
+//import { ProgressSpinnerModule } from 'primeng/progressspinner';
+//import { RadioButtonModule } from 'primeng/radiobutton';
+//import { ScrollPanelModule } from 'primeng/scrollpanel';
+//import { ScrollerModule } from 'primeng/scroller';
+//import { SelectButtonModule } from 'primeng/selectbutton';
+//import { SidebarModule } from 'primeng/sidebar';
+//import { SliderModule } from 'primeng/slider';
+//import { SpeedDialModule } from 'primeng/speeddial';
+//import { SplitButtonModule } from 'primeng/splitbutton';
+//import { StepperModule } from 'primeng/stepper';
+//import { StepsModule } from 'primeng/steps';
+//import { TabViewModule } from 'primeng/tabview';
+//import { TableModule } from 'primeng/table';
+//import { TieredMenuModule } from 'primeng/tieredmenu';
+//import { TimelineModule } from 'primeng/timeline';
+//import { ToastModule } from 'primeng/toast';
+//import { ToggleButtonModule } from 'primeng/togglebutton';
+//import { ToolbarModule } from 'primeng/toolbar';
+//import { TooltipModule } from 'primeng/tooltip';
+//import { TreeModule } from 'primeng/tree';
+//import { TreeSelectModule } from 'primeng/treeselect';
+//import { TreeTableModule } from 'primeng/treetable';
+//import { TriStateCheckboxModule } from 'primeng/tristatecheckbox';
+
+let modules = [ ButtonModule, CheckboxModule, InputNumberModule, InputTextModule, InputTextareaModule ];
+
+@NgModule({
+    imports: modules,
+    providers: [ ConfirmationService ],
+    exports: modules
+})
+export class PrimeNGModule {}
