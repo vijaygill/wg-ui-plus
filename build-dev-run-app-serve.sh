2bb07cdc754f7dbeac0380270b6120178e7ea83b
thecoshmabn <git@thecoshman.com>
2024-05-04 18:14:14 +0100

    Update scripts to use conventional .gitconfig from home folder

--- build-dev-run-app-serve.sh
+++ build-dev-run-app-serve.sh
@@ -4,4 +4,4 @@ BASE_DIR="$(dirname "$(readlink -f "${BASH_SOURCE}")")"
 
 source ${BASE_DIR}/build-dev.sh
 
-docker run -it --rm -p 13080:80 -v "${BASE_DIR}":/wg-ui-plus -v "${BASE_DIR}/gitconfig":/home/pi/.gitconfig:ro -v /tmp:/tmp -v /var/run/docker.sock:/var/run/docker.sock wg-ui-plus /wg-ui-plus/scripts/run-app.sh
+docker run -it --rm -p 13080:80 -v "${BASE_DIR}":/wg-ui-plus -v "${HOME}/.gitconfig":/home/pi/.gitconfig:ro -v /tmp:/tmp -v /var/run/docker.sock:/var/run/docker.sock wg-ui-plus /wg-ui-plus/scripts/run-app.sh
--- build-dev-run-ng-build.sh
+++ build-dev-run-ng-build.sh
@@ -4,4 +4,4 @@ BASE_DIR="$(dirname "$(readlink -f "${BASH_SOURCE}")")"
 
 source ${BASE_DIR}/build-dev.sh
 
-docker run -it --rm -v "${BASE_DIR}":/wg-ui-plus -v "${BASE_DIR}/gitconfig":/home/pi/.gitconfig:ro -v /tmp:/tmp wg-ui-plus /wg-ui-plus/scripts/ng-build.sh
+docker run -it --rm -v "${BASE_DIR}":/wg-ui-plus -v "${HOME}/.gitconfig":/home/pi/.gitconfig:ro -v /tmp:/tmp wg-ui-plus /wg-ui-plus/scripts/ng-build.sh
