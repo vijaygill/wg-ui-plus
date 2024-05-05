8df5d052db3cf5552073a3242725750c788226ab
thecoshman <git@thecoshman.com>
2024-05-05 12:33:45 +0100

    Restore dev shell script and refactor scripts a bit
    Also give names to the dev containers
    Closes #5

--- build-dev-run-ng-build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-BASE_DIR="$(dirname "$(readlink -f "${BASH_SOURCE}")")"
-
-source ${BASE_DIR}/build-dev.sh
-
-docker run -it --rm -v "${BASE_DIR}":/wg-ui-plus -v "${HOME}/.gitconfig":/home/pi/.gitconfig:ro -v /tmp:/tmp wg-ui-plus /wg-ui-plus/scripts/ng-build.sh
