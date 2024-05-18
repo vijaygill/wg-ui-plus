865521c2afc94183ef6abc769dd0cebcb5c73b75
Vijay Gill <Vijay.Gill@hotmail.com>
2024-05-18 22:35:11 +0100

    More work done.

--- src/mainapp/admin.py
+++ src/mainapp/admin.py
@@ -8,6 +8,14 @@ class PeerAdmin(admin.ModelAdmin):
 class PeerGroupAdmin(admin.ModelAdmin):
   list_display = ("name", "description", "disabled")
 
+  def get_fields(self, request, obj=None):
+    return ['name', 'description', 'peers', 'targets']
+
+  def get_readonly_fields(self, request, obj=None):
+    if obj and not obj.allow_modify_self:
+        return ['name', 'description']
+    return []
+
 class TargetAdmin(admin.ModelAdmin):
   list_display = ("name", "description", "ip_address", "port", "disabled")
 
--- src/mainapp/models.py
+++ src/mainapp/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+
 
 # Create your models here.
 class PeerGroup(models.Model):
@@ -8,6 +11,7 @@ class PeerGroup(models.Model):
   allow_modify_self = models.BooleanField(null = True)
   allow_modify_peers = models.BooleanField(null = True)
   allow_modify_targets = models.BooleanField(null = True)
+  peers = models.ManyToManyField('Peer', blank=True)
   targets = models.ManyToManyField('Target', blank=True)
 
   def __str__(self):
@@ -17,24 +21,32 @@ class Peer(models.Model):
   name = models.CharField(max_length = 255)
   description = models.CharField(max_length = 255, null = True)
   disabled = models.BooleanField(null = True)
-  ip_address = models.CharField(max_length = 255, null = True)
-  port = models.IntegerField(null = True)
-  public_key = models.CharField(max_length = 255, null = True)
-  private_key = models.CharField(max_length = 255, null = True)
-  peer_group = models.ForeignKey(PeerGroup, on_delete=models.CASCADE, null = True)
+  ip_address = models.CharField(max_length = 255, null = True, blank=True)
+  port = models.IntegerField(null = True, blank=True)
+  public_key = models.CharField(max_length = 255, null = True, blank=True)
+  private_key = models.CharField(max_length = 255, null = True, blank=True)
+  peer_groups = models.ManyToManyField('PeerGroup', blank=True)
 
   def __str__(self):
      return f"{self.name} - {self.description}"
+  
+  def save(self, force_insert=False, force_update=False):
+    pg = PeerGroup.objects.filter(name = 'Everyone')
+    if pg:
+      self.peer_group.add(pg[0])
+    super(Peer, self).save(force_insert, force_update)
 
 class Target(models.Model):
   name = models.CharField(max_length = 255)
-  description = models.CharField(max_length = 255, null = True)
+  description = models.CharField(max_length = 255, null = True, blank=True)
   disabled = models.BooleanField(null = True)
-  ip_address = models.CharField(max_length = 255, null = True)
-  port = models.IntegerField(null = True)
+  ip_address = models.CharField(max_length = 255, null = True, blank=True)
+  port = models.IntegerField(null = True, blank=True)
   allow_modify_self = models.BooleanField(null = True)
   allow_modify_peergroups = models.BooleanField(null = True)
   peer_groups = models.ManyToManyField('PeerGroup', blank=True)
 
   def __str__(self):
      return f"{self.name} - {self.description}"
+
+
