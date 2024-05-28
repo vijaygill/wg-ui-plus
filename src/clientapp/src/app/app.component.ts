3ecbd5db5de1cb57c327801375c45786e0bf744a
Vijay Gill <Vijay.Gill@hotmail.com>
2024-05-28 17:37:31 +0100

    added PrimeNg configuration code from their setup guide.

--- src/clientapp/src/app/app.component.ts
+++ src/clientapp/src/app/app.component.ts
@@ -5,14 +5,26 @@ import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { SidepanelComponent } from './sidepanel/sidepanel.component';
 import { AppSharedModule } from './app-shared.module';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, RouterOutlet, FormsModule, SidepanelComponent, AppSharedModule ],
+  imports: [RouterModule, RouterOutlet, FormsModule, SidepanelComponent, AppSharedModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'wg-ui-plus';
+
+  constructor(private primengConfig: PrimeNGConfig) { }
+
+  ngOnInit() {
+    // this.primengConfig.zIndex = {
+    //   modal: 1100,    // dialog, sidebar
+    //   overlay: 1000,  // dropdown, overlaypanel
+    //   menu: 1000,     // overlay menus
+    //   tooltip: 1100   // tooltip
+    // };
+  }
 }
