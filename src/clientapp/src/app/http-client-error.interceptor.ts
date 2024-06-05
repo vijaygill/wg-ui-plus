587313f8b32b36df5fca297e74b0753306cb4562
Vijay Gill <Vijay.Gill@hotmail.com>
2024-06-05 23:12:45 +0100

    Improved error messages in http interceptor.

--- src/clientapp/src/app/http-client-error.interceptor.ts
+++ src/clientapp/src/app/http-client-error.interceptor.ts
@@ -11,6 +11,7 @@ import { ConfirmationDialogService } from './confirmation-dialog-service';
 import { MessageService } from 'primeng/api';
 import { WebapiService } from './webapi.service';
 import { ServerStatus } from './webapi.entities';
+import { ErrorStateMatcher } from '@angular/material/core';
 
 @Injectable({
   providedIn: 'root',
@@ -19,13 +20,24 @@ export class HttpClientErrorInterceptor implements HttpInterceptor {
 
   constructor(private webapiService: WebapiService) { }
 
+  private httpErrors: { [status: number]: string } = {
+    0: 'Error while communicating with server.',
+    401: 'Unauthorised call to server.',
+    403: 'Client is forbidden from calling server.',
+    404: 'Client is forbidden from calling server.',
+    500: 'Internal Server Error.',
+  }
+
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(request)
       .pipe(
         catchError((error: HttpErrorResponse) => {
-          if (error.status <= 0) {
-            this.webapiService.pushServerStatus({ status: 'error', message: 'Error while communicating with server.' } as ServerStatus)
-          }
+          let message = 'Server returned HTTP Error '
+            + error.status
+            + '. '
+            + (error.status in this.httpErrors ? this.httpErrors[error.status] : 'Please check logs on server side.');
+
+          this.webapiService.pushServerStatus({ status: 'error', message: message } as ServerStatus)
           return throwError(error);
         })
       );
