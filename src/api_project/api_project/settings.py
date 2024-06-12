43b11f5b64890c48128fba6220f9c801e3e79404
Vijay Gill <Vijay.Gill@hotmail.com>
2024-06-12 18:26:58 +0100

    Removed TokenAuthentication from DEFAULT_AUTHENTICATION_CLASSES.

--- src/api_project/api_project/settings.py
+++ src/api_project/api_project/settings.py
@@ -115,7 +115,6 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.SessionAuthentication",
-        "rest_framework.authentication.TokenAuthentication",
     ),
 }
 
