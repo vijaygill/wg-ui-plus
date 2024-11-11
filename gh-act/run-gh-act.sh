81bc28c39229d2b75e46c4c7d991114bb8866876
Vijay Gill <Vijay.Gill@hotmail.com>
2024-11-11 22:21:38 +0000

    Changed path to app in run-gh-act.sh

--- gh-act/run-gh-act.sh
+++ gh-act/run-gh-act.sh
@@ -6,6 +6,6 @@ then
 else
     IMAGE_TAG="wg-ui-plus-gh-actions"
     docker build --build-arg GITHUB_TOKEN="${GITHUB_TOKEN}" -t "${IMAGE_TAG}" -f Dockerfile .
-    docker run -it --rm --env GITHUB_TOKEN="${GITHUB_TOKEN}" --env GH_TOKEN="${GITHUB_TOKEN}" -v "${PWD}":/app:ro -v /var/run/docker.sock:/var/run/docker.sock ${IMAGE_TAG} bash
+    docker run -it --rm --env GITHUB_TOKEN="${GITHUB_TOKEN}" --env GH_TOKEN="${GITHUB_TOKEN}" -v "${PWD}/..":/app:ro -v /var/run/docker.sock:/var/run/docker.sock ${IMAGE_TAG} bash
 fi
 
