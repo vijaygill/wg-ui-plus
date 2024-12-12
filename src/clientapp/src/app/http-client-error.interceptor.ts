3b938ba3ede8dad864161ca16d5d45d88e9c06e4
Vijay Gill <Vijay.Gill@hotmail.com>
2024-12-12 17:56:22 +0000

    Improved error handling in http interceptor.

--- src/clientapp/src/app/http-client-error.interceptor.ts
+++ src/clientapp/src/app/http-client-error.interceptor.ts
@@ -17,31 +17,46 @@ export class HttpClientErrorInterceptor implements HttpInterceptor {
 
   constructor(private webapiService: WebapiService) { }
 
-  private httpErrors: { [status: number]: string } = {
-    0: 'Error while communicating with server.',
-    401: 'Unauthorised call to server.',
-    403: 'Client is forbidden from calling server.',
-    404: 'Client is forbidden from calling server.',
-    500: 'Internal Server Error.',
+  private httpErrors: httpError[] = [
+    { from: 0, to: 0, message: 'Error while communicating with server.' ,},
+    { from: 401, to: 401, message: 'Unauthorised call to server.' ,},
+    { from: 403, to: 403, message: 'Client is forbidden from calling server.' ,},
+    { from: 404, to: 404, message: 'Resource not found.',},
+    { from: 407, to: 407, message: 'Proxy Authentication Required.', },
+    { from: 408, to: 408, message: 'Request Timeout.',},
+    { from: 500, to: 500, message: 'Internal Server Error.'},
+    { from: 501, to: 501, message: 'Not Implemented.'},
+    { from: 502, to: 502, message: 'Bad Gateway. Possibly the reverse proxy cannot see upstream server.'},
+    { from: 503, to: 599, message: 'Other error.'},
+  ];
+
+  getHttpError(statusCode: number): string | null {
+    let x = this.httpErrors.find(item => item.from <= statusCode && statusCode <= item.to);
+    let res = x ? x.message : null;
+    return res;
   }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(request)
       .pipe(
         catchError((error: HttpErrorResponse) => {
-          if (!(error.status in this.httpErrors)) {
-            return throwError(() => error);
-          }
+          let errorMessageFromList = this.getHttpError(error.status);
           let m = error.error && error.error.message ? error.error.message : '';
           let message = 'Server returned HTTP Error '
             + error.status
             + '. '
             + (m ? m + '. ' : '')
-            + (error.status in this.httpErrors ? this.httpErrors[error.status] : 'Please check logs on server side.');
+            + (errorMessageFromList ? errorMessageFromList : 'Please check logs on server side.');
 
           this.webapiService.pushServerStatus({ status: 'error', message: message } as ServerStatus)
           return throwError(() => new Error(message));
         })
       );
   }
+}
+
+interface httpError {
+  from: number,
+  to: number,
+  message: string
 }
