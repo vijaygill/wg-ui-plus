11fe93b80e0c2768bd202a0db5fe36b9b535c0bd
Vijay Gill <Vijay.Gill@hotmail.com>
2024-12-19 00:23:23 +0000

    More work done on migration of PrimeNG

--- src/clientapp/src/app/app.config.ts
+++ src/clientapp/src/app/app.config.ts
@@ -24,7 +24,7 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-        preset: Lara,
+        preset: Aura,
         options: {
           darkModeSelector: '.my-app-dark',
         }
